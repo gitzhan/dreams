3c1e12ddbcac5f77f931bec7b2b398c8e6f8ac88
gitzhan <260597116@qq.com>
2018-06-06 16:27:20 +0800

    base common

---
+++ gaia/cloud/dreams-common/common-boot/src/main/java/com/dreams/cloud/CommonBootApplication.java
@@ -0,0 +1,12 @@
+package com.dreams.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonBootApplication.class, args);
+	}
+}
