e5bad6061679948f375c356ff8ee98cace6df74d
james <james@gmail.com>
2018-06-17 12:54:47 +0800

    server docker

--- server/cloud/eurekaserver/src/main/java/com/dreams/cloud/eureka/ServerApplication.java
+++ server/cloud/eurekaserver/src/main/java/com/dreams/cloud/eureka/ServerApplication.java
@@ -1,4 +1,4 @@
-package com.dreams.cloud;
+package com.dreams.cloud.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaserverApplication {
+public class ServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaserverApplication.class, args);
+		SpringApplication.run(ServerApplication.class, args);
 	}
 }
